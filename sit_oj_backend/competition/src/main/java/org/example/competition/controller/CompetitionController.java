2c227100956b39b390a6d6b9e72795d6687d5ddb
oldcatsthecat <924849252@qq.com>
2026-01-13 15:51:24 +0800

    debug competiton

--- sit_oj_backend/competition/src/main/java/org/example/competition/controller/CompetitionController.java
+++ sit_oj_backend/competition/src/main/java/org/example/competition/controller/CompetitionController.java
@@ -81,7 +81,7 @@ public class CompetitionController {
             return competitionService.handleSubmission(userId, competitionId, problemId, code, language);
 
         } catch (Exception e) {
-            return Result.error("提交数据格式错误，请检查题目ID等参数");
+            return Result.error("提交数据格式错误，请检查题目ID等参数" + e.getMessage());
         }
     }
 
